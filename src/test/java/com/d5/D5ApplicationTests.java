23df4bfe941d1c826f74fc0f0bb4db7cb2427aac
hyuans <2472613445@qq.com>
2025-09-18 13:22:16 +0800

    Initial commit: Spring Boot HelloWorld with Security

---
+++ src/test/java/com/d5/D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
